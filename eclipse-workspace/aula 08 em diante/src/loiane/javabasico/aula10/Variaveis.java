ae4b6eeb4db248a66b4e11e3f145342c2bd290c0
Emerson <emperiuz@gmail.com>
2018-06-24 21:25:02 -0300

    add aula 10
    Convençao de nome de variaveis e classes; package (nome com pontos, que cria várias pastas)

---
+++ eclipse-workspace/aula 08 em diante/src/loiane/javabasico/aula10/Variaveis.java
@@ -0,0 +1,12 @@
+package loiane.javabasico.aula10;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		int idade;
+		String nome;
+		idade = 25;
+		nome = "Eu";
+		System.out.println("Idade: " + idade + ", nome: " + nome);
+	}
+}
+
